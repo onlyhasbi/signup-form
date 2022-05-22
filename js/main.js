25d053e6a55101373ab7af795dff5c484c32cd92
A MUH HASBI H <onlyhasbi@gmail.com>
2022-05-22 23:34:51 +0800

    fix error notification and make script more simple

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
+const input = document.querySelectorAll(".input");
+const message = document.querySelectorAll(".message");
+const form = document.querySelector(".form");
+
+const isEmpty = (value) => {
+  return (result = !value.trim().length);
+};
+
+input.forEach((item, i) => {
+  item.addEventListener("input", (e) => {
+    const target = e.currentTarget;
+
+    if (!!target.value.trim().length) {
+      target.classList.remove("alert-icon");
+      if (target.name === "email") target.placeholder = "email address";
+      message[i].classList.remove("error");
+    }
+  });
+});
+
 (() => {
   "use strict";
-
-  var input = document.querySelectorAll(".input");
-  var message = document.querySelectorAll(".message");
-
-  var form = document.querySelector(".form");
 
   form.addEventListener(
     "submit",
@@ -12,50 +27,25 @@
       e.preventDefault();
       e.stopPropagation();
 
-      var firstNameValidation = e.target[0].value.trim().length > 0;
-      var lastNameValidation = e.target[1].value.trim().length > 0;
-      var emailValidation = e.target[2].value.trim().length > 0;
-      var passwordValidation = e.target[3].value.trim().length > 0;
-
-      if (!firstNameValidation) {
-        message[0].classList.add("error");
-        input[0].classList.add("alert-icon");
-        input[0].placeholder = "";
-      } else {
-        message[0].classList.remove("error");
-        input[0].classList.remove("alert-icon");
-      }
-
-      if (!lastNameValidation) {
-        message[1].classList.add("error");
-        input[1].classList.add("alert-icon");
-        input[1].placeholder = "";
-      } else {
-        message[1].classList.remove("error");
-        input[1].classList.remove("alert-icon");
-      }
-
-      if (!emailValidation) {
-        message[2].classList.add("error");
-        input[2].classList.add("alert-icon");
-        input[2].placeholder = "email@example.com";
-      } else {
-        if (form.checkValidity()) {
-          message[2].classList.remove("error");
-          input[2].classList.remove("alert-icon");
+      const target = e.currentTarget;
+      for (var index = 0; index < target.length - 1; index++) {
+        if (isEmpty(target[index].value)) {
+          target[index].classList.add("alert-icon");
+          message[index].classList.add("error");
+          if (target[index].name === "email") {
+            target[index].placeholder = "email@example.com";
+            message[index].textContent = "Email cannot be empty";
+          }
         } else {
-          message[2].textContent = "Looks like this is not an email";
-        }
-      }
+          target[index].classList.remove("alert-icon");
+          message[index].classList.remove("error");
 
-      if (!passwordValidation) {
-        message[3].classList.add("error");
-        input[3].classList.add("alert-icon");
-        input[3].placeholder = "";
-        input[3].value = "";
-      } else {
-        message[3].classList.remove("error");
-        input[3].classList.remove("alert-icon");
+          if (target[index].name === "email" && !form.checkValidity()) {
+            target[index].classList.add("alert-icon");
+            message[index].classList.add("error");
+            message[index].textContent = "looks like this is not an email";
+          }
+        }
       }
     },
     false
